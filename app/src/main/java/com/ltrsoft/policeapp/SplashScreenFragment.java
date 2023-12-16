52073d589ebeb9a2b2e2bd0774621dc3fde695c2
Shaikh kaif <shaikhkaif7014@gmail.com>
2023-12-16 18:38:16 +0530

    commit by kaif

--- app/src/main/java/com/ltrsoft/policeapp/SplashScreenFragment.java
+++ app/src/main/java/com/ltrsoft/policeapp/SplashScreenFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class SplashScreenFragment extends Fragment {
     public SplashScreenFragment() {}
@@ -14,6 +15,7 @@ public class SplashScreenFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.splash_screen_fragment, container, false);
+        Toast.makeText(getContext(), "Flash Screen", Toast.LENGTH_SHORT).show();
         return view;
     }
 }
