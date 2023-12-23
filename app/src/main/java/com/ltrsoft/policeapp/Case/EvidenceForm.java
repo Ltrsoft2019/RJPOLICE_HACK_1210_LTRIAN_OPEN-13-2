2d6730bbaefc13952d50c46414f46c84f2a61ae5
Shaikh kaif <shaikhkaif7014@gmail.com>
2023-12-23 14:17:43 +0530

    Evidence Form victim Form added by kaif

--- app/src/main/java/com/ltrsoft/policeapp/Case/EvidenceForm.java
+++ app/src/main/java/com/ltrsoft/policeapp/Case/EvidenceForm.java
@@ -1,22 +1,51 @@
 package com.ltrsoft.policeapp.Case;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.ltrsoft.policeapp.LoinRegistration.LoginFragment;
 import com.ltrsoft.policeapp.LoinRegistration.RegistrationFragment;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import com.ltrsoft.policeapp.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EvidenceForm extends Fragment {
     public EvidenceForm() {    }
-    private TextView gallery,camera;
+    private TextView gallery,camera,ev_name,e_desc;
     private EditText ename,edesc;
     private ImageView back;
+    private Button e_save;
+
+    private static final String BASE_URL = "https://rj.ltr-soft.com/public/police_api/evidance/create_evidance.php";
+
+    private String firid="2023-12-14-1";
+    private ProgressBar progressBar2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -25,17 +54,101 @@ public class EvidenceForm extends Fragment {
         gallery = view.findViewById(R.id.gallery);
         camera = view.findViewById(R.id.camera);
         back = view.findViewById(R.id.back1_btn);
-        ename = view.findViewById(R.id.ename);
-        edesc = view.findViewById(R.id.edesc);
+        ev_name=view.findViewById(R.id.ev_name);
+        e_desc=view.findViewById(R.id.e_desc);
+        e_save=view.findViewById(R.id.e_save);
+        progressBar2=view.findViewById(R.id.progressBar2);
+        progressBar2.setVisibility(View.INVISIBLE);
+
+        e_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar2.setVisibility(View.VISIBLE);
+                e_save.setVisibility(View.INVISIBLE);
+                if (ev_name.getText().toString().isEmpty()) {
+                    ev_name.setError("Enter Evidence Name");
+                    progressBar2.setVisibility(View.INVISIBLE);
+                    e_save.setVisibility(View.VISIBLE);
+                } else {
+                    ev_name.setError(null);
+                    if(e_desc.getText().toString().isEmpty()){
+                        e_desc.setError("Enter Description");
+                        progressBar2.setVisibility(View.INVISIBLE);
+                        e_save.setVisibility(View.VISIBLE);
+                    }
+                    else {
+                        e_desc.setError(null);
+                        senddata();
+                    }
+                }
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 getActivity().getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container,new InvestigationFormFragment())
                         .commit();
             }
         });
+
+
         return view;
+
+        }
+
+    public void senddata(){
+        RequestQueue requestQueue= Volley.newRequestQueue(getContext());
+        StringRequest stringRequest =new StringRequest(Request.Method.POST,
+                BASE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                showDoneDialog(getContext());
+
+                progressBar2.setVisibility(View.GONE);
+                e_save.setVisibility(View.VISIBLE);
+                e_desc.setText("");
+                ev_name.setText("");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+
+
+            public void onErrorResponse(VolleyError error) {
+                progressBar2.setVisibility(View.INVISIBLE);
+                e_save.setVisibility(View.VISIBLE);
+                Toast.makeText(getContext(), ""+error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map=new HashMap<>();
+                map.put("fir_id",firid.toString());
+                map.put("evidance_name",ev_name.getText().toString());
+                map.put("evidance_description",e_desc.getText().toString());
+
+                return map;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
+
+    private void showDoneDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Evidence ")
+                .setMessage("Evidence Saved Successfully")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        dialog.dismiss();
+                    }
+                });
+
+        // Create and display the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
--- app/src/main/java/com/ltrsoft/policeapp/Case/VictimFormFragment.java
+++ app/src/main/java/com/ltrsoft/policeapp/Case/VictimFormFragment.java
@@ -2,20 +2,169 @@ package com.ltrsoft.policeapp.Case;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.ltrsoft.policeapp.LoinRegistration.LoginFragment;
+import com.ltrsoft.policeapp.Navigation.NavigationFragment;
 import com.ltrsoft.policeapp.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class VictimFormFragment extends Fragment {
-    public VictimFormFragment() {
-    }
+
+    private EditText vv_firname,vv_fname,vv_mname,v_lname,vf_dob,v_mobile,v_address,v_email,v_contryname,v_statename,
+            v_distname,v_cityname,v_addhar;
+
+    private Button v_submit;
+
+    private RadioButton v_male_btn,v_female_btn;
+    private String gender;
+    private ProgressBar v_progressbar;
+
+    private String firid="2023-12-14-1";
+
+    private RadioGroup v_radioGroup;
+
+    private static final String BASE_URL = "https://rj.ltr-soft.com/public/police_api/investigation_victim/create_investigation_victim.php";
+
+    private ImageView v_back;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.victim_form_form, container, false);
+
+        vv_firname=view.findViewById(R.id.vv_firname);
+        vv_fname=view.findViewById(R.id.vv_fname);
+        vv_mname=view.findViewById(R.id.vv_mname);
+        v_lname=view.findViewById(R.id.v_lname);
+        vf_dob=view.findViewById(R.id.vf_dob);
+        v_mobile=view.findViewById(R.id.v_mobile);
+        v_address=view.findViewById(R.id.v_address);
+        v_email=view.findViewById(R.id.v_email);
+        v_contryname=view.findViewById(R.id.v_contryname);
+        v_statename=view.findViewById(R.id.v_statename);
+        v_distname=view.findViewById(R.id.v_distname);
+        v_cityname=view.findViewById(R.id.v_cityname);
+        v_addhar=view.findViewById(R.id.v_addhar);
+        v_submit=view.findViewById(R.id.v_submit);
+        v_male_btn=view.findViewById(R.id.v_male_btn);
+        v_female_btn=view.findViewById(R.id.v_female_btn);
+        v_progressbar=view.findViewById(R.id.v_progressbar);
+        v_radioGroup= view.findViewById(R.id.v_radioGroup);
+        v_back=view.findViewById(R.id.v_back);
+
+
+        v_progressbar.setVisibility(View.INVISIBLE);
+
+        v_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .addToBackStack(null) .replace(R.id.main_container,new NavigationFragment())
+                        .commit();
+            }
+        });
+        v_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(v_male_btn.isChecked()){
+                    gender=v_male_btn.getText().toString();
+                } else if (v_female_btn.isChecked()) {
+                    gender=v_female_btn.getText().toString();
+
+                }
+                else {
+                    Toast.makeText(getContext(), "Please Select Gender", Toast.LENGTH_SHORT).show();
+                }
+                senddata();
+            }
+        });
+
+
         return view;
     }
+
+
+    public void senddata(){
+        RequestQueue requestQueue= Volley.newRequestQueue(getContext());
+        StringRequest stringRequest =new StringRequest(Request.Method.POST,
+                BASE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getContext(), "Data Saved Successfully", Toast.LENGTH_LONG).show();
+
+                v_progressbar.setVisibility(View.GONE);
+                v_submit.setVisibility(View.VISIBLE);
+
+                vv_firname.setText("");
+                 vv_fname.setText("");
+                vv_mname.setText("");
+                v_lname.setText("");
+                vf_dob.setText("");
+                v_mobile.setText("");
+                v_address.setText("");
+                v_email.setText("");
+                v_contryname.setText("");
+                v_statename.setText("");
+                v_distname.setText("");
+                v_cityname.setText("");
+                v_addhar.setText("");
+                v_radioGroup.clearCheck();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+
+
+            public void onErrorResponse(VolleyError error) {
+                v_progressbar.setVisibility(View.INVISIBLE);
+                v_submit.setVisibility(View.VISIBLE);
+                Toast.makeText(getContext(), ""+error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map=new HashMap<>();
+
+                map.put("",vv_firname.getText().toString());
+                map.put("victim_fname",vv_fname.getText().toString());
+                map.put("victim_mname",vv_mname.getText().toString());
+                map.put("victim_lname",v_lname.getText().toString());
+                map.put("victim_dob",vf_dob.getText().toString());
+                map.put("victim_mobile_no",v_mobile.getText().toString());
+                map.put("victim_address",v_address.getText().toString());
+                map.put("victim_email",v_email.getText().toString());
+                map.put("country_id",v_contryname.getText().toString());
+                map.put("state_id",v_statename.getText().toString());
+                map.put("district_id",v_distname.getText().toString());
+                map.put("city_id",v_cityname.getText().toString());
+                map.put("victim_adhar",v_addhar.getText().toString());
+                map.put("fir_id",firid.toString());
+                return map;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
 }
